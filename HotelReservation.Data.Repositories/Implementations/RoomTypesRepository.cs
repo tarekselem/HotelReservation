77b8c72d34b675cb3254a0bf9ebe2944834cd197
tarekselem <t.hamdy91@yahoo.com>
2018-01-20 13:56:39 +0200

    Repository&UOW pattern

---
+++ HotelReservation.Data.Repositories/Implementations/RoomTypesRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using HotelReservation.Models.Entities;
+using HotelReservation.Data.Common;
+
+namespace HotelReservation.Data.Repositories.Implementations
+{
+    public class RoomTypesRepository : IRoomTypesRepository
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public RoomTypesRepository(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<RoomType> GetAllRoomTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RoomType GetRoomType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HotelReservation.Data.SQL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HotelReservation.Data.SQL
+{
+    public class Class1
+    {
+    }
+}
