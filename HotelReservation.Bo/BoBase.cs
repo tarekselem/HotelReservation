93984ae4532ad62ad8f54fd0fb324446a9981269
Mostafa Sakr <mostafa.sakr4@gmail.com>
2018-01-21 12:56:02 +0100

    add BoReservation

---
+++ HotelReservation.Bo/BoBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HotelReservation.Bo
+{
+    public class BoBase<Entity>
+    {
+        public BoBase()
+        {
+        }
+    }
+}
---
+++ HotelReservation.Bo/BoReservation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HotelReservation.Models.Entities;
+
+namespace HotelReservation.Bo
+{
+    public class BoReservation : BoBase<DtoReservation>
+    {
+        public BoReservation()
+        {
+             
+        }
+        public void Book(DtoReservation reservation)
+        {
+            // roles: 
+            // number of days must be more than number of cancelation days fees
+        }
+        public void Checkout(DtoReservation reservation)
+        {
+            // roles: 
+            // mark reservation as checkedout
+            // Hotel write down remaining amount to the Guest Account
+
+        }
+        public void CheckIn(DtoReservation reservation)
+        {
+            //Marks that reservation as "Checked in"
+        }
+
+        public void Cancel(DtoReservation reservation)
+        {
+            //hotel posts a cancellation fee to the Guest Account
+        }
+
+    }
+}
